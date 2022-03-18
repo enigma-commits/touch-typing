1b514612767a874e054f663a000bfd0ce5ebee0c
akhilesh singh <akhileshsingh2314@gmail.com>
2022-03-19 00:12:19 +0530

    added the link on the brand logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ export function Header() {
   return (
     <div className="header-parent">
       <div className="header-first-child">
-        <a href="#" className="header-brand-button">
+        <a
+          href="https://enigma-commits.github.io/touch-typing/"
+          className="header-brand-button"
+        >
           touch-typing
         </a>
       </div>
