6946044f6efdd7e120aca7a289c1d7e416e6cf08
akhilesh singh <akhileshsingh2314@gmail.com>
2022-03-18 23:25:53 +0530

    created a basic site

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+export function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer-text">Made for The ♡ with Touch-Typing </p>
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+export function Header() {
+  return (
+    <div className="header-parent">
+      <div className="header-first-child">
+        <a href="#" className="header-brand-button">
+          touch-typing
+        </a>
+      </div>
+    </div>
+  );
+}
